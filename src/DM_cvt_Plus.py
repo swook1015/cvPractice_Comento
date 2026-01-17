186b2c26d8f474262c50df65ec33a6165e2ff247
asd <swook5062@gmail.com>
2026-01-17 19:10:28 +0900

    코드수정

--- src/DM_cvt_Plus.py
+++ src/DM_cvt_Plus.py
@@ -16,16 +16,24 @@ if ims:
         image = cv2.imread(i)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         depth_map =  cv2.applyColorMap(gray, cv2.COLORMAP_JET)
+
+        base_name = os.path.basename(i)
+        save_path = f'output/{base_name}'
+
         # 3D 포인트 클라우드 변환
         h, w = depth_map.shape[:2]
         X, Y = np.meshgrid(np.arange(w), np.arange(h))
         Z = gray.astype(np.float32) # Depth 값을 Z 축으로 사용
         # 3D 좌표 생성
         points_3d = np.dstack((X, Y, Z))
+
+        save_point = f'output/{base_name}_3d_Points.npy'
+        np.save(save_point, points_3d)
+
         # 결과 이미지 저장 (추가)
-        cv2.imwrite('../output/result2.jpg', depth_map)
-        # 결과 출력
-        cv2.imshow('Depth Map', depth_map)
+        cv2.imwrite(save_path, depth_map)
+        # 결과 출력 (추가)
+        cv2.imshow(save_path, depth_map)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 else:
