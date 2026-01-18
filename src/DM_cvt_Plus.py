70a5c83cb18dcc9d8a20988e2cf2880475c64579
asd <swook5062@gmail.com>
2026-01-18 17:51:15 +0900

    제출 전 최종 수정 주석추가

--- src/DM_cvt_Plus.py
+++ src/DM_cvt_Plus.py
@@ -4,21 +4,21 @@ import numpy as np
 import os
 import glob
 
-if not os.path.exists('output'):
+if not os.path.exists('output'): # 해당 output 파일폴더가 없으면 생성
     print('메인폴더에 output 파일이 없으므로 생성')
     os.makedirs('output')
 
-ims = glob.glob('data/*.jpg')
+ims = glob.glob('data/*.jpg') # data 파일 안에 있는 .jpg를 ims에 할당
 
-if ims:
-    print(f'{len(ims)} 개의 이미지를 감지했습니다.')
-    for i in ims:
+if ims: #프로그램이 data 파일 폴더 안에 있는 .jpg를 감지했으면 실행
+    print(f'{len(ims)} 개의 이미지를 감지했습니다.')# 사용자에게 프로그램이 몇 개의 이미지를 감지했는지 보여줌 (추가)
+    for i in ims:# 모든 파일에 적용하기 위해 for문 실행
         image = cv2.imread(i)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         depth_map =  cv2.applyColorMap(gray, cv2.COLORMAP_JET)
 
-        base_name = os.path.basename(i)
-        save_path = f'output/{base_name}'
+        base_name = os.path.basename(i) #해당 파일 이름 저장
+        save_path = f'output/{base_name}'#해당 파일 경로 저장
 
         # 3D 포인트 클라우드 변환
         h, w = depth_map.shape[:2]
@@ -27,8 +27,8 @@ if ims:
         # 3D 좌표 생성
         points_3d = np.dstack((X, Y, Z))
 
-        save_point = f'output/{base_name}_3d_Points.npy'
-        np.save(save_point, points_3d)
+        save_point = f'output/{base_name}_3d_Points.npy' #3D포인트를 저장하기 위한 경로 저장
+        np.save(save_point, points_3d) #3D포인트 저장
 
         # 결과 이미지 저장 (추가)
         cv2.imwrite(save_path, depth_map)
